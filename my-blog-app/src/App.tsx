968c9724bea7c4cf1e7f4aa2d3f6e6c53428ac6a
kokotyan <kokochi.0207@gmail.com>
2024-02-21 14:34:37 +0900

    初コミット

---
+++ my-blog-app/src/App.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+export default function App() {
+  const [posts, setPosts] = useState<any[]>([]); 
+
+  return (
+    <div>
+      {/* ここに JSX コンテンツを記述 */}
+    </div>
+  );
+}
